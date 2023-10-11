9bff914d1f979ec58cfc4241f82e2cb018d02ac3
bombies <xjamesdzn@gmail.com>
2023-10-11 11:46:50 -0500

    Update nickname command to support gradients

--- src/main/java/me/bombies/learningplugin/utils/messages/MessageUtils.java
+++ src/main/java/me/bombies/learningplugin/utils/messages/MessageUtils.java
@@ -1,11 +1,12 @@
 package me.bombies.learningplugin.utils.messages;
 
+import me.bombies.learningplugin.LearningPlugin;
 import org.bukkit.ChatColor;
 
 import java.util.regex.Pattern;
 
 public class MessageUtils {
-    private final static Pattern COLOUR_CODE_REGEX = Pattern.compile("(?i)&([0-9A-FK-OR])");
+    private final static Pattern COLOUR_CODE_REGEX = Pattern.compile("(?i)&([0-9a-fA-Fk-oK-OrRxX])|&x([0-9A-Fa-f]{6})|&x([0-9A-Fa-f]{3})|&#([0-9A-Fa-f]{6})");
 
     public static String color(String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
